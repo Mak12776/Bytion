72e80cc0221cc6205b6ce5bf3a0ef881ea588108
Mak12776 <mak12776@gmail.com>
2017-04-27 15:11:17 +0430

    Update build.cmd

--- source/build.cmd
+++ source/build.cmd
@@ -1,4 +1,4 @@
 @echo off
-gcc -Wextra -o bytion main.c
+gcc -Wextra -O3 -o bytion main.c
 IF %ERRORLEVEL% EQU 0 echo ---- Build successful ----
 IF %ERRORLEVEL% EQU 9009 echo ---- gcc does not exist ----
