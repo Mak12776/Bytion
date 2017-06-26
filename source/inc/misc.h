c8d9146d59bac47d102eab2410804dfc44c9d382
mak12776 <mak12776@gmail.com>
2017-06-26 05:05:32 +0430

    fixed combination of 'extern' and 'inline' for some functions changes 	modified:   build 	renamed:    inc/misc.c -> inc/misc.h 	deleted:    inc/terminal_color.c 	modified:   inc/terminal_color.h

--- source/inc/misc.h
+++ source/inc/misc.h
@@ -7,10 +7,106 @@
 #include "boolean.h"
 #include "error_numbers.h"
 
-extern inline bool Compare(const char *str1,const char *str2);
-extern inline uint ConvertToReal(const char *str);
-extern inline void ToReal(uchar in, uchar *out);
-extern inline void ToHex(uchar in, uchar *out);
-extern inline void ToChar(uchar in, uchar *out);
+extern inline uint ConvertToReal(const char *str)
+{
+  #ifdef _DEBUG
+  if (!str)
+  {
+    fprintf(stderr, "in %s: NULL pointer !\n", __func__);
+    return 0;
+  }
+  #endif
+  uint result=0;
+  if ((*str)>48 && (*str < 58))
+  {
+    result+=(*(str++))-48;
+  }
+  else
+  {
+    errno=EILSEQ;
+    return 0;
+  }
+  while (*str)
+  {
+    if ((*str)>47 && (*str < 58))
+    {
+      result=result*10+(*str++)-48;
+      continue;
+    }
+    errno=EILSEQ;
+    return 0;
+  }
+  return result;
+}
+
+extern inline bool Compare(const char *str1,const char *str2)
+{
+  #ifdef _DEBUG
+  if ((!str1) || (!str2))
+  {
+    fprintf(stderr, "in %s: NULL pointer !\n", __func__);
+    return false;
+  }
+  #endif
+  while ( *str1 == *str2 )
+  {
+    if (!( *(str1++) | *(str2++) ) )
+      return true;
+  }
+  return false;
+}
+
+extern inline inline void ToReal(uchar in, uchar *out)
+{
+  if (in>0)
+  {
+    *(out+2)=(in%10)+48;
+    in/=10;
+    if (in>0)
+    {
+      *(out+1)=(in%10)+48;
+      in/=10;
+      if (in>0)
+      {
+        *(out)=(in%10)+48;
+        return;
+      }
+    *(out)=' ';
+    return;
+    }
+  *(out+1)=' ';
+  *(out)=' ';
+  return;
+  }
+  *(out+2)='0';
+  *(out+1)=' ';
+  *(out)=' ';
+}
+
+extern inline void ToChar(uchar in, uchar *out)
+{
+  *(out)=in;
+  *(out+1)=' ';
+}
+
+extern inline void ToHex(uchar in, uchar *out)
+{
+  if ((in/16)<10)
+  {
+    *out=in/16+48;
+  }
+  else
+  {
+    *out=in/16+55;
+  }
+  if ((in%16)<10)
+  {
+    *(out+1)=in%16+48;
+  }
+  else
+  {
+    *(out+1)=in%16+55;
+  }
+}
 
 #endif
--- source/inc/terminal_color.h
+++ source/inc/terminal_color.h
@@ -44,7 +44,41 @@
 
 #endif
 
-extern inline void SetColor(const char clr);
-extern inline void printColored(const char *input, char clr);
+char _terminal_color_fore=COLOR_ENV;
+
+extern inline void SetColor(const char clr)
+{
+  #ifdef __linux__
+  if (_terminal_color_fore!=clr)
+  {
+    printf("\033[%dm", _terminal_color_fore=clr);
+  }
+  #elif defined _WIN32 || defined _WIN64
+  if (_terminal_color_fore!=clr)
+  {
+    SetConsoleTextAttribute(hConsole, _terminal_color_fore=clr);
+  }
+  #endif
+}
+
+extern inline void printColored(const char *input, const char clr)
+{
+  #ifdef __linux__
+  if (_terminal_color_fore==clr)
+  {
+    fputs(input, stdout);
+  }
+  else
+  {
+    printf("\033[%dm%s", _terminal_color_fore=clr, input);
+  }
+  #elif defined _WIN32 || defined _WIN64
+  if (_terminal_color_fore!=clr)
+  {
+  SetConsoleTextAttribute(hConsole, _terminal_color_fore=clr);
+  }
+  fputs(input, stdout);
+  #endif
+}
 
 #endif
